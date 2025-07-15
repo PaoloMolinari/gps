5558d6381c20ae55783d395b46122f5f912355a1
p3mb <149981774+PaoloMolinari@users.noreply.github.com>
2025-07-15 19:56:46 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,25 @@
 
-// Options for map
+//var canvas;
 
-/*
-var options = {
-  lat: 0,
-  lng: 0,
-  zoom: 4,
-  style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-}
+//tabla
 
-var mappa = new Mappa('Leaflet');
-var myMap;
-*/
+let myTable;
+let collectedDataTable;
+let dataTable = [];
+let bSave;
 
-var canvas;
+let pbutton;
 
 
+//interface variable
+let myButton;
+let saveCsvButton;
+let myInput;
+let myText;
+let stuff;
+let bgColor;
 
+// geolocation variables
 let lat;
 let lon;
 let alt;
@@ -26,9 +29,13 @@ let heading;
 let speed;
 let tstamp;
 
-let data = [];
 
-let table;
+function preload(){
+    myTable = loadTable('data.csv', 'csv', 'header');
+    console.log(myTable);
+ //  myTable = loadTable('data.csv');    
+}
+
 
 function getposition(position) {
     lat = position.coords.latitude;
@@ -38,16 +45,76 @@ function getposition(position) {
     altacc = position.coords.altitudeAccuracy;
     heading = position.coords.heading;
     speed = position.coords.speed;
-    tstamp = position.timestamp;    
+    tstamp = position.timestamp;
+
+    // timestamp is defined as the number of seconds since the beginning of the Unix epoch, which is January 1st, 1970, at midnight (UTC). To change it to time:
+
+    /*
+      let unix_timestamp = 1549312452;
+
+      // Create a new JavaScript Date object based on the timestamp
+      // multiplied by 1000 so that the argument is in milliseconds, not seconds
+      var date = new Date(unix_timestamp * 1000);
+
+      // Hours part from the timestamp
+      var hours = date.getHours();
+
+      // Minutes part from the timestamp
+      var minutes = "0" + date.getMinutes();
+
+      // Seconds part from the timestamp
+      var seconds = "0" + date.getSeconds();
+
+      // Will display time in 10:30:23 format
+      var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+      console.log(formattedTime);
+    */
+    
 }
 
-function setup() {    
+/*
+function drawButton() {
+  Fill();
+  stroke(4);
+  rect(150, 200, 100, 50);
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text("Presiona aquí", 200, 235);
+}
+*/
+
+function setup() {
+   
     canvas = createCanvas(800, 700);
     fill(70, 203,31);	
     stroke(100);
+/*
+    collectedDataTable = new p5.Table();
+    collectedDataTable.addColumn('id');
+    collectedDataTable.addColumn('feel');
+    collectedDataTable.addColumn('lat');
+    collectedDataTable.addColumn('lon');
+    collectedDataTable.addColumn('alt');
+    collectedDataTable.addColumn('acc');
+    collectedDataTable.addColumn('altacc');
+    collectedDataTable.addColumn('head');
+    collectedDataTable.addColumn('speed');
+    collectedDataTable.addColumn('tstamp');
+*/
+  //  print(myTable.getColumn("tstamp"));
 
-    table = new p5.Table();
+    //read data in the table
 
+    for (let i = 0; i < myTable.getRowCount(); i++) {
+	for (let j = 0; j < myTable.getColumnCount(); j++) {
+	    print(myTable.get(i, j));
+	}
+    }
+    
+    //geodata initialization
+ 
     lat = 0;
     lon = 0;
     alt = 0;
@@ -57,20 +124,42 @@ function setup() {
     speed = 0;
     tstamp = 0;
 
-    // noLoop();
+    myButton = createButton('Add Data');
+    myInput = createInput('Enter a feeling');
+    saveCsvButton = createButton('Save data')
+    myInput.position(30, 220);
+    myButton.position(35 + myInput.width, 220);
+    saveCsvButton.position(35 + myInput.width, 250);
+
+    // what is the callback for the button?
+    myButton.mousePressed(adddata);
+//    bSave = false;
+
+    saveCsvButton.mousePressed(savedata);
+    
+    // .input() is a calback for something is being entered
+    myInput.input(typing);
+    
+    // store the text to reuse and display realtime
+    myText = '';
+    stuff='';
+
+ //   drawButton();
+
+    //noLoop();
 }
 
 function draw() {
     background(0);
     sleep(5000);
-    console.log('.');
+   // console.log('.');
     navigator.geolocation.getCurrentPosition(getposition);
-    console.log(lat, lon, alt, acc, altacc, heading, speed, tstamp);
+  //  console.log(lat, lon, alt, acc, altacc, heading, speed, tstamp);
 
 
 //    if(lat != nill && lon != nill){
-	let la = nf(lat);
-	let lo = nf(lon);
+//	let la = nf(lat);
+//	let lo = nf(lon);
   //  }
     
     textSize(22);
@@ -84,34 +173,63 @@ function draw() {
     text('speed: ' +  nf(speed), 30, 170);
     text('time stamp: ' +  nf(tstamp), 30, 195);
 
+   // text('real time input: ' + stuff, 30, 270);
+    // text(myText, 30, 295);
+
+    text(myTable.getRowCount() + ' set de datos agregados.', 30, 300);
+
 }
 
 function mouseClicked() {
-  // Code to run.
+    // Code to run.
+  //  bSave = true;
 }
 
-function sleep(millisecondsDuration)
-{
+function sleep(millisecondsDuration){
   return new Promise((resolve) => {
     setTimeout(resolve, millisecondsDuration);
   })
 }
 
+function typing() {
+    stuff = this.value();
+}
+
+function adddata() {
+    // cast as a color when assigning it to the variable
+    //bgColor = color(myInput.value());
+//    bSave = true;
+ //   dataTable = [];
+ //   dataTable.push(myInput.value(), lat, lon, alt, acc, altacc, heading, speed, tstamp);
+
+    let newRow = myTable.addRow();
+    newRow.setNum('id', myTable.getRowCount() - 1);
+    newRow.setString('feel', myInput.value());
+    newRow.setNum('lat', lat);
+    newRow.setNum('lon', lon);
+    newRow.setNum('alt', alt);
+    newRow.setNum('acc', acc);
+    newRow.setNum('accalt', altacc);
+    newRow.setNum('head', heading);
+    newRow.setNum('speed', speed);
+    newRow.setNum('tstamp', tstamp);
+
+    //saveTable(myTable, 'new.csv');
+    
+}
+
+function savedata(){
+
+saveTable(myTable, 'data.csv');
+
+    
+}
+
+
+
+
+
 /*
-
-	navigator.geolocation.getCurrentPosition(
-
-    // Success callback
-    function(position) {
-			background(220);
-			textSize(32);
-			text("latitude: " + position.coords.latitude, 5, 100);
-			text("longitude: " + position.coords.longitude, 5, 200);
-
-        
-	position is an object containing various information about
-	the acquired device location:
-
 	position = {
 	    coords: {
 		latitude - Geographical latitude in decimal degrees.
